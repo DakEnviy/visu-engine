b3dc1a24acc2b7eec9016fc8caa72c314801c4f2
Golbakh Mikhail <golbah@yandex.ru>
2016-10-09 20:35:24 +0500

    Starter pack

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="vinyl/vinyl.d.ts" />
+/// <reference path="gulp-typescript/gulp-typescript.d.ts" />
